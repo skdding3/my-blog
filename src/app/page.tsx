9c576bca2f62d21078e1ed9ead3283d19a544c09
skdding3 <skdding3@gmail.com>
2024-09-09 14:33:11 +0900

    [feat] provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,26 +5,9 @@ import { MoreStories } from "@/app/_components/more-stories";
 import { getAllPosts } from "@/lib/api";
 
 export default function Index() {
-  const allPosts = getAllPosts();
-
-  const heroPost = allPosts[0];
-
-  const morePosts = allPosts.slice(1);
-
   return (
-    <main>
-      <Container>
-        <Intro />
-        <HeroPost
-          title={heroPost.title}
-          coverImage={heroPost.coverImage}
-          date={heroPost.date}
-          author={heroPost.author}
-          slug={heroPost.slug}
-          excerpt={heroPost.excerpt}
-        />
-        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-      </Container>
+    <main className="flex items-center justify-center py-20">
+      <p className="text-xl font-semibold"> SUnki's Blog</p>
     </main>
   );
 }
---
+++ src/components/providers.tsx
@@ -0,0 +1,54 @@
+"use client"; // Client Side 동작
+
+import { createContext, useEffect, useRef } from "react";
+import { ThemeProvider, useTheme } from "next-themes";
+import { usePathname } from "next/navigation";
+
+// 제네릭 타입으로 인한 함수 선언식 변경
+function usePrevious<T>(value: T) {
+  let ref = useRef<T>(value);
+
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+
+  return ref.current;
+}
+
+const ThemeWatcher = () => {
+  let { resolvedTheme, setTheme } = useTheme();
+  useEffect(() => {
+    let media = window.matchMedia("(prefers-color-scheme: dark)");
+
+    const onMediaChange = () => {
+      let systemTheme = media.matches ? "dark" : "light";
+      if (resolvedTheme === systemTheme) {
+        setTheme("system");
+      }
+    };
+
+    onMediaChange();
+    media.addEventListener("change", onMediaChange);
+
+    return () => {
+      media.removeEventListener("change", onMediaChange);
+    };
+  }, [resolvedTheme, setTheme]);
+
+  return null;
+};
+
+export const AppContext = createContext<{ previousPathname?: string }>({});
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  let pathname = usePathname();
+  let previousPathname = usePrevious(pathname);
+  return (
+    <AppContext.Provider value={{ previousPathname }}>
+      <ThemeProvider attribute="class" disableTransitionOnChange>
+        <ThemeWatcher />
+        {children}
+      </ThemeProvider>
+    </AppContext.Provider>
+  );
+};
